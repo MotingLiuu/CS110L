4c01e3f22dcc50dfbbf1a4d618580efbe566dbfb
MotingLiuu <moting.liuu@gmail.com>
2026-06-07 12:45:37 +0800

    updated to v3 minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,17 +1,85 @@
 use std::error::Error;
 use std::fs;
+use std::env;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+    
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase(); // This would create a new String, not a &str
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+   results
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
+    
+    let results = if config.case_sensitive {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 // fn parse_config(args: &[String]) -> Config{
@@ -26,10 +94,15 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err(); // if CASE_INSENSITIVE is set
+                                                                 // var would return Ok containing the value
+                                                                 // otherwise it would return Err
         Ok(
             Config {
                 query,
                 filename,
+                case_sensitive,
             }
         )
     }
